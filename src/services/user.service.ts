a41e87fc7f2afbcf943964d998f2d7f3df5923be
Gipotalamus <hectop23@ukr.net>
2016-12-25 19:50:15 +0200

    user service added but empty yet

---
+++ src/services/user.service.ts
@@ -0,0 +1,13 @@
+import {Http} from "@angular/http";
+
+export class UserService {
+
+  constructor(private http: Http) {
+  }
+
+  login(name: string, password: string) {
+  }
+
+  logout() {
+  }
+}
